788b2101ffc7875aedb4bbdfbaafabcd96b0398e
Jedison Vieira Junior <jedison.vieira@hotmail.com>
2022-12-17 11:18:01 -0300

    score api ip fixing

--- services/ScoreAPI.js
+++ services/ScoreAPI.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const  baseURL=  'http://192.168.0.109:3000/matches'
-
+const  baseURL=  'http://10.0.2.2:3000/matches'
 
 export const findAllScores = async () => {
   console.log("Iniciando busca de partidas")
